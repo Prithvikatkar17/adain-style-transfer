e0d72588f7cac4583a5248d487d2aacd6b3ff4ee
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-09-26 12:43:39 +0530

    feat : vgg dencoder

--- NST_Code/utils/models.py
+++ NST_Code/utils/models.py
@@ -83,3 +83,42 @@ class VGGEncoder(nn.Module):
         if is_test:
             return h4 
         return [h1, h2, h3, h4]
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.decoder = nn.Sequential(
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(512, 256, (3, 3)),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(256, 256, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(256, 256, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(256, 256, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(256, 128, (3, 3)),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(128, 128, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(128, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(64, 64, (3, 3)),
+            nn.ReLU(),
+            nn.ReflectionPad2d(1,1,1,1),
+            nn.Conv2d(64, 3, (3, 3))
+        )
+
+    def forward(self,x):
+        return self.decoder(x)
